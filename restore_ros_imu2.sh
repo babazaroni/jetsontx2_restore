0851198d48fdf624db06436af794b6537002a363
babazaroni <chuck@sanna.com>
2018-05-05 15:44:06 +0000

    ros imu

--- restore_ros_imu2.sh
+++ restore_ros_imu2.sh
@@ -6,5 +6,26 @@ cd src
 git clone https://github.com/jetsonhacks/rtimulib_ros
 cd rtimulib_ros/src
 
-#echo 
+#echo change "imu/data" to "imu"
+read -p "Press any key to continue..." -n1  -s
 gedit rtimulib_ros.cpp
+
+cd ~/catkin_ws
+
+catkin_make
+
+#setup apt to install ros packages
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+# install the visualizer
+# instal from indigo was not found, use kinetic
+#sudo apt-get install ros-indigo-razor-imu-9dof
+sudo apt-get install ros-kinetic-razor-imu-9dof
+
+sudo apt-get install python-visual
+sudo apt-get install python-wxtools
+
+# see http://wiki.ros.org/razor_imu_9dof
+
+#install arduino IDE
+
