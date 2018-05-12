6d97cb6e6ffdd842fb3f01a3cfcc420294cbd1ed
babazaroni <chuck@sanna.com>
2018-05-12 15:07:10 +0000

    added opencv script

---
+++ restore_opencv.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+# see https://github.com/jetsonhacks/buildOpenCVTX2
+# License: MIT. See license file in root directory
+# Copyright(c) JetsonHacks (2017-2018)
+
+INSTALL_PYTHON2=ON
+INSTALL_PYTHON3=ON
+
+
+cd $HOME
+sudo apt-get install -y \
+    libglew-dev \
+    libtiff5-dev \
+    zlib1g-dev \
+    libjpeg-dev \
+    libpng12-dev \
+    libjasper-dev \
+    libavcodec-dev \
+    libavformat-dev \
+    libavutil-dev \
+    libpostproc-dev \
+    libswscale-dev \
+    libeigen3-dev \
+    libtbb-dev \
+    libgtk2.0-dev \
+    cmake \
+    pkg-config./
+
+# Python 2.7
+sudo apt-get install -y python-dev python-numpy python-py python-pytest -y
+
+
+
+# GStreamer support
+sudo apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev 
+
+git clone https://github.com/opencv/opencv.git
+cd opencv
+git checkout 3.4.1
+# This is for the test data
+cd $HOME
+git clone https://github.com/opencv/opencv_extra.git
+cd opencv_extra
+git checkout 3.4.1
+
+cd $HOME/opencv
+mkdir build
+cd build
+# Jetson TX2 
+cmake \
+    -D CMAKE_BUILD_TYPE=Release \
+    -D CMAKE_INSTALL_PREFIX=/usr \
+    -D BUILD_PNG=OFF \
+    -D BUILD_TIFF=OFF \
+    -D BUILD_TBB=OFF \
+    -D BUILD_JPEG=OFF \
+    -D BUILD_JASPER=OFF \
+    -D BUILD_ZLIB=OFF \
+    -D BUILD_EXAMPLES=ON \
+    -D BUILD_opencv_java=OFF \
+    -D BUILD_opencv_python2=ON \
+    -D BUILD_opencv_python3=OFF \
+    -D ENABLE_PRECOMPILED_HEADERS=OFF \
+    -D WITH_OPENCL=OFF \
+    -D WITH_OPENMP=OFF \
+    -D WITH_FFMPEG=ON \
+    -D WITH_GSTREAMER=ON \
+    -D WITH_GSTREAMER_0_10=OFF \
+    -D WITH_CUDA=ON \
+    -D WITH_GTK=ON \
+    -D WITH_VTK=OFF \
+    -D WITH_TBB=ON \
+    -D WITH_1394=OFF \
+    -D WITH_OPENEXR=OFF \
+    -D CUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-8.0 \
+    -D CUDA_ARCH_BIN=6.2 \
+    -D CUDA_ARCH_PTX="" \
+    -D INSTALL_C_EXAMPLES=ON \
+    -D INSTALL_TESTS=ON \
+    -D OPENCV_TEST_DATA_PATH=../opencv_extra/testdata \
+    ../
+
+# Consider using all 6 cores; $ sudo nvpmodel -m 2 or $ sudo nvpmodel -m 0
+NUM_CPU=$(nproc)
+make -j$(($NUM_CPU - 1))
+sudo make install
+cd $HOME
+rm -rf opencv*
+
