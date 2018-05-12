1a4a5ce5aa73d4451774f806af3db77cf6ee8fe1
babazaroni <chuck@sanna.com>
2018-05-12 03:32:57 +0000

    changed i2c access

--- restore_ros_imu1.sh
+++ restore_ros_imu1.sh
@@ -6,15 +6,16 @@ cd
 
 sudo apt-get install cmake-curses-gui
 
-#edit 90-i2c.rules and add KERNEL=="i2c-[0-7]",MODE="0666"
-echo "edit 90-i2c.rules and add "
-echo "KERNEL==\"i2c-[0-7]\",MODE=\"0666\""
-echo "Press 'g' to generate and exit"
-read -p "Press any key to continue..." -n1  -s
-
-sudo gedit /etc/udev/urles.c/90-i2c.rules
 #the following did not work
 #sudo bash -c 'echo \''KERNEL==\"i2c-[0-7]\",MODE=\"0666\"\''> /etc/udev/rules.d/90-i2c.rules'
+
+#use instead these instructions at https://forum.up-community.org/discussion/2141/tutorial-gpio-i2c-spi-access-without-root-permissions
+
+#For I2C-bus access create the file /etc/udev/rules.d/50-i2c.rules with the following contents:
+#SUBSYSTEM=="i2c-dev", GROUP="i2cuser", MODE="0660"
+sudo groupadd i2cuser
+sudo adduser "$USER" i2cuser
+
 
 #should already have installed the RTIMUlib script which mkdir bebop/translation
 
