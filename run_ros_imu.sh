b963ab0472f966a2c88053e74b87bbcbe943b865
babazaroni <chuck@sanna.com>
2018-05-15 15:53:00 +0000

    run_rus_imu.sh

--- run_ros_imu.sh
+++ run_ros_imu.sh
@@ -4,10 +4,12 @@
 # eval "$BASH_POST_RC"
 
 BASH_POST_RC='cd /home/$(whoami)/catkin_ws;source devel/setup.bash; roscore' gnome-terminal
-sleep .5
+sleep 1
 BASH_POST_RC='cd /home/$(whoami)/catkin_ws;source devel/setup.bash; roslaunch rtimulib_ros rtimulib_ros.launch' gnome-terminal
 
 sleep .5
 BASH_POST_RC='cd /home/$(whoami)/catkin_ws;source devel/setup.bash; roslaunch razor_imu_9dof razor-display.launch' gnome-terminal
 
+sleep .5
+BASH_POST_RC='cd /home/$(whoami)/catkin_ws;source devel/setup.bash;roslaunch robot_localization translate_ekf.launch' gnome-terminal
 
